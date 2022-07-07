d78baadd502fe3d0b5b82621a2e3d26ee03a0d10
Vittorioemot <105027649+Vittorioemot@users.noreply.github.com>
2022-07-07 09:55:23 +0800

    worked on index.js to see questions and be able to answer in the console

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Provide a brief description about the project.',
+        name: 'description'
+    },
+    {
+        type: 'list',
+        message: 'Choose the license for your project?',
+        name: 'license',
+        choices: ['MIT', 'Mozilla', 'ISC', 'IBM']
+    }
+];
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions).then(function (data) {
+        console.log(data);
+
+        console.log(data.title);
+        console.log(data.description);
+        console.log(data.license);
+    })
+
+}
 
 // Function call to initialize app
 init();
-
-console.log(123456);
